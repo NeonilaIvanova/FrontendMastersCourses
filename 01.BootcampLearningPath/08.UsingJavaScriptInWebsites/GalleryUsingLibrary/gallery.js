2731a98900ef34644804d5fca90886dc70b40872
Neonila <neonilaiv@gmail.com>
2020-12-19 15:19:24 +0200

    Finished with Gallery using Swiper

--- 01.BootcampLearningPath/08.UsingJavaScriptInWebsites/GalleryUsingLibrary/gallery.js
+++ 01.BootcampLearningPath/08.UsingJavaScriptInWebsites/GalleryUsingLibrary/gallery.js
@@ -1,9 +1,35 @@
 new Swiper(".swiper-container", {
     speed: 400,
     spaceBetween: 100,
+    loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     }
 });
 
+// navigation physics
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const emoji = document.querySelector('.brand');
+const divStyler = styler(emoji);
+const emojiXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(emoji, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(emojiXY.get()).start(emojiXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: emojiXY.get(),
+      velocity: emojiXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 500,
+      mass: 1,
+      damping: 10
+    }).start(emojiXY);
+  });
