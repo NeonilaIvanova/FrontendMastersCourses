5f388166dbb4835eb3170b8645b26bfb164b7e70
Neonila <neonilaiv@gmail.com>
2020-12-19 19:58:11 +0200

    Finished with Ajax project

--- 02.YourPathToBecomingACareer-ReadyWebDeveloper/01.CompleteIntroToWebDevelopment:V2Course/04.AjaxProject/doggos.js
+++ 02.YourPathToBecomingACareer-ReadyWebDeveloper/01.CompleteIntroToWebDevelopment:V2Course/04.AjaxProject/doggos.js
@@ -1,40 +1,78 @@
-const DOG_URL = "https://dog.ceo/api/breeds/image/random";
-//const BREEDS = "https://dog.ceo/api/breeds/list/all"
+//const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
 
-const doggos = document.querySelector(".doggos");
-//const breeds = document.querySelector(".breeds");
+const select = document.querySelector('.breeds');
+const spinner = document.querySelector('.spinner');
+const img = document.querySelector('.dog-img');
+//const addDoggoButton = document.querySelector(".add-doggo");
 
-/* function selectBreed() {
-    const promise = fetch(BREEDS);
-    promise
-        .then(function(response) {
-            const processingPromise = response.json();
-            return processingPromise;
+function createSelect () {
+    fetch(BREEDS_URL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        const breedsObject = data.message;
+        const breedsArray = Object.keys(breedsObject);
+
+        breedsArray.forEach(function (breed) {
+            const option = document.createElement('option');
+            option.value = breed;
+            option.innerText = breed;
+            select.appendChild(option);
         })
-        .then(function(processedResponse) {
-            processedResponse.Each((breed => {
-                console.log(breed);
-                const option = document.createElement("option");
-                option.value = processedResponse.message;
-                option.innerText = processedResponse.message;
-            }))
-        });
-} */
-
-function addNewDoggo() {
-    const promise = fetch(DOG_URL);
-    promise
-        .then(function(response) {
-            const processingPromise = response.json();
-            return processingPromise;
-        })
-        .then(function(processedResponse) {
-            const img = document.createElement("img");
-            img.src = processedResponse.message;
-            img.alt = "Cute doggo";
-            doggos.prepend(img);
-        });
+    })
 }
 
-document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
-selectBreed();
+function createURL (event) {
+    if (event.target.value === "default ") {
+        return;
+    } else {
+    const chosenBreed = event.target.value;
+    let urlForChosenBreed = `https://dog.ceo/api/breed/${chosenBreed}/images/random`;
+    addDoggoByBreed(urlForChosenBreed);
+    }       
+}
+
+/* function addDoggo () {
+    fetch(DOG_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function(data) {
+        const img = document.createElement('img');
+        img.src = data.message;
+        img.alt = 'Cute dogo';
+        
+        document.querySelector('.doggos').prepend(img);
+    })    
+} */
+
+function addDoggoByBreed (urlForChosenBreed) {
+    spinner.classList.add('show');
+    img.classList.remove('show');
+    fetch(urlForChosenBreed)
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function(data){
+        img.src = data.message;
+        console.log(img.src);
+        img.alt = 'Cute dogo';
+        document.querySelector('.doggo').appendChild(img);
+    })
+}
+
+img.addEventListener("load", function () {
+    spinner.classList.remove('show');
+    img.classList.add("show");
+})
+
+function init () {
+    // addDoggoButton.addEventListener("click", addDoggo);
+    select.addEventListener("change", createURL);
+    createSelect();
+
+}
+
+init();
